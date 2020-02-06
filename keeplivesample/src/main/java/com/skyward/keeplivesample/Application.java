3829054744776bae012defe6a28bc3059ccab429
skyward <1224085894@qq.com>
2020-02-06 15:18:49 +0800

    把源码弄成Android Studio项目结构

---
+++ keeplivesample/src/main/java/com/skyward/keeplivesample/Application.java
@@ -0,0 +1,48 @@
+package com.skyward.keeplivesample;
+
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.skyward.keeplive.KeepLive;
+import com.skyward.keeplive.config.ForegroundNotification;
+import com.skyward.keeplive.config.ForegroundNotificationClickListener;
+import com.skyward.keeplive.config.KeepLiveService;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //定义前台服务的默认样式。即标题、描述和图标
+        ForegroundNotification foregroundNotification = new ForegroundNotification("测试","描述", R.mipmap.ic_launcher,
+                //定义前台服务的通知点击事件
+                new ForegroundNotificationClickListener() {
+
+                    @Override
+                    public void foregroundNotificationClick(Context context, Intent intent) {
+                    }
+                });
+        //启动保活服务
+        KeepLive.startWork(this, KeepLive.RunMode.ROGUE, foregroundNotification,
+                //你需要保活的服务，如socket连接、定时任务等，建议不用匿名内部类的方式在这里写
+                new KeepLiveService() {
+                    /**
+                     * 运行中
+                     * 由于服务可能会多次自动启动，该方法可能重复调用
+                     */
+                    @Override
+                    public void onWorking() {
+
+                    }
+                    /**
+                     * 服务终止
+                     * 由于服务可能会被多次终止，该方法可能重复调用，需同onWorking配套使用，如注册和注销broadcast
+                     */
+                    @Override
+                    public void onStop() {
+
+                    }
+                }
+        );
+    }
+}
